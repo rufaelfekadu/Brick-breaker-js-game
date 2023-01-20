3c294d1a9e45e9e8d6b76e68885c9fce55d9660c
Omar3ain <omar3ain@gmail.com>
2023-01-20 10:59:00 +0200

    making the paddle move with keyboard or mouse move

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
-
+let leftArrow=false;
+let rightArrow=false;
 
 class Paddle{
     constructor(width, height){
@@ -9,23 +10,64 @@ class Paddle{
         this.height = height;
         this.x = canvas.width/2 - this.width/2
         this.y = canvas.height - this.height - this.margin_bottom;
-        this.dx = 5
+        this.dx = 20;
     }
-
     draw(){
-        ctx.fillStyle = 'black';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = "#2e3548";
+        ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
     }
 }
 
 const paddle = new Paddle(150,20);
-paddle.draw();
+
 
 class Ball{}
 class Brick{}
 
+
+canvas.addEventListener("mousemove", function(event){
+        if(event.clientX <= paddle.width/2){
+            paddle.x = 0
+        }else if(event.clientX >= canvas.width - paddle.width/2){
+            paddle.x = canvas.width-paddle.width;
+        }else{
+            paddle.x = event.clientX -paddle.width/2
+        } 
+});
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "ArrowLeft"){
+        leftArrow = true;
+    }else if(event.key== "ArrowRight"){
+        rightArrow = true;
+    }
+});
+document.addEventListener("keyup", function(event){
+    if(event.key === "ArrowLeft"){
+        leftArrow = false;
+    }else if(event.key== "ArrowRight"){
+        rightArrow = false;
+    }
+});
+function movePaddle(){
+    if(rightArrow && paddle.x + paddle.width < canvas.width){
+        paddle.x += paddle.dx;
+    }else if(leftArrow && paddle.x > 0){
+        paddle.x -= paddle.dx;
+    }
+}
+
+function update(){
+    movePaddle();
+}
+function drawGame(){
+    paddle.draw();
+}
+
 function tick(){
-    ctx.clear(0,0,canvas.width , canvas.height)
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawGame()
+    update();
     requestAnimationFrame(tick);
 }
 tick();
