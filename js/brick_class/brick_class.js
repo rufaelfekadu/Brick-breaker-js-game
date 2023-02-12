9969c6f60254d2352bce7692e8c671f7543a0c8d
RawanAboegila-ITI43 <rawan.moyasser.aboegila@gmail.com>
2023-02-12 16:19:12 +0200

    "Updated brick images file and audio media to better match the look and feel "

--- js/brick_class/brick_class.js
+++ js/brick_class/brick_class.js
@@ -1,30 +1,27 @@
 import * as Canvas from '../index.js';
 
-class Brick
-{
-    constructor(width,height,x,y,brick_strength , power , hasPower , powerActive)
-    {
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
+class Brick {
+    constructor(width, height, x, y, brick_strength, power, hasPower, powerActive) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
         this.fillColor = "#2e3548";
-        this.strokeColor="#fff";
+        this.strokeColor = "#fff";
         this.brick_strength = brick_strength;
         this.power = power;
-        this.powerActive=powerActive;
+        this.powerActive = powerActive;
         this.hasPower = hasPower;
         this.image3 = document.getElementById("img_brick3");
         this.image2 = document.getElementById("img_brick2");
         this.image1 = document.getElementById("img_brick");
     }
 
-    DrawBrick()
-    {
-        Canvas.ctx.fillStyle=this.fillColor;
-        Canvas.ctx.fillRect(this.x ,this.y ,this.width , this.height );
+    DrawBrick() {
+        Canvas.ctx.fillStyle = this.fillColor;
+        Canvas.ctx.fillRect(this.x, this.y, this.width, this.height);
         Canvas.ctx.strockStyle = this.strokeColor;
-        Canvas.ctx.strokeRect(this.x , this.y ,this.width , this.height);
+        Canvas.ctx.strokeRect(this.x, this.y, this.width, this.height);
     }
 }
 
